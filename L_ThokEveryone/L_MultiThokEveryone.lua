79155e8bfd5f3ed94a460d1f97fc22cd88acc35c
Anirudh Sevugan <sonicforces207@gmail.com>
2025-03-23 22:19:33 +0530

    Update L_MultiThokEveryone.lua

--- L_ThokEveryone/L_MultiThokEveryone.lua
+++ L_ThokEveryone/L_MultiThokEveryone.lua
@@ -2,52 +2,39 @@
 local PF_EXECUTED_SPECIAL = 0x10000  -- Arbitrary value that doesn't conflict with existing flags
 
 addHook("AbilitySpecial", function(player)
-    -- Check if the player is using a default character skin or specific character
-    if player.mo.skin ~= "sonic" and player.mo.skin ~= "tails" and player.mo.skin ~= "knuckles" and
-       player.mo.skin ~= "amy" and player.mo.skin ~= "fang" and player.mo.skin ~= "metalsonic" and
-       player.mo.skin ~= "sonic_tails" then
-        -- If the player is not using one of the specified skins, skip the thok
-        return
-    end
-    
-    -- Make sure the player hasn't already executed a special ability
+    -- Ensure the player hasn't already executed a special ability
     if player.pflags & PF_EXECUTED_SPECIAL then
         return true
     end
-    
-    -- Force the player to perform a thok special ability
+
+    -- Force the player to perform a Thok special ability
     local actionspd = FixedMul(player.mo.scale, 60 * FRACUNIT)  -- Adjust for size
     
     -- If the player is underwater, reduce speed
     if player.mo.eflags & MFE_UNDERWATER then
         actionspd = actionspd / 2
     end
-    
-    -- Perform the thok thrust action
-    P_InstaThrust(player.mo, player.mo.angle, actionspd)
-    
-    -- Spawn the thok mobj (the projectile representing the thok)
-    P_SpawnThokMobj(player)
-    
-    -- Play the thok sound effect
-    S_StartSound(player.mo, sfx_thok)
 
+    -- Set PF_NOJUMPDAMAGE before the Thok occurs for proper collision handling
     player.pflags = $ & ~PF_NOJUMPDAMAGE  
 
-    -- Uncomment the line below to disable the multithok.
-    -- player.pflags = $|PF_EXECUTED_SPECIAL 
+    -- Perform the Thok thrust action
+    P_InstaThrust(player.mo, player.mo.angle, actionspd)
+    
+    -- Spawn the Thok projectile
+    P_SpawnThokMobj(player)
+    
+    -- Play the Thok sound effect
+    S_StartSound(player.mo, sfx_thok)
+
+    -- Uncomment the line below to disable the multithok
+    --player.pflags = $ | PF_EXECUTED_SPECIAL
     
     -- Prevent the original ability code from running (no default special ability)
     return true
 end)
 
-
-
-
-
 -- Reset the executed special flag when the player spawns
 addHook("PlayerSpawn", function(player)
-    player.pflags = player.pflags & ~PF_EXECUTED_SPECIAL  -- Reset the executed special flag when the player spawns
-    player.pflags = $ & ~PF_NOJUMPDAMAGE
+    player.pflags = player.pflags & ~PF_EXECUTED_SPECIAL  -- Reset executed flag
 end)
-
