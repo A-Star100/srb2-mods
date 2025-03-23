67659613a777c1659d6a181097d555e9df8a5732
Anirudh Sevugan <sonicforces207@gmail.com>
2025-03-23 21:43:31 +0530

    Update L_ThokEveryone.lua

--- L_ThokEveryone/L_ThokEveryone.lua
+++ L_ThokEveryone/L_ThokEveryone.lua
@@ -3,6 +3,7 @@ local PF_EXECUTED_SPECIAL = 0x10000  -- Arbitrary value that doesn't conflict wi
 
 addHook("AbilitySpecial", function(player)
     -- Check if the player is using a default character skin or specific character
+    -- You can comment out the if condition below to allow characters added via mods to thok.
     if player.mo.skin ~= "tails" and player.mo.skin ~= "knuckles" and
        player.mo.skin ~= "amy" and player.mo.skin ~= "fang" and player.mo.skin ~= "metalsonic" and
        player.mo.skin ~= "sonic_tails" then
