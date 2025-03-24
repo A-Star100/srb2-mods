dc570068fb8ec64fcca7d5239dfdf6b76bfca2c3
Anirudh Sevugan <sonicforces207@gmail.com>
2025-03-24 17:42:06 +0530

    Update L_ThokEveryone.lua

--- L_ThokEveryone/L_ThokEveryone.lua
+++ L_ThokEveryone/L_ThokEveryone.lua
@@ -23,8 +23,10 @@ addHook("AbilitySpecial", function(player)
         actionspd = actionspd / 2
     end
 
+    player.mo.state = S_PLAY_ROLL
+
     -- Set PF_NOJUMPDAMAGE before the Thok occurs for proper collision handling
-    --player.pflags = $ & ~PF_NOJUMPDAMAGE  
+    player.pflags = $ & ~PF_NOJUMPDAMAGE  
 
     -- Perform the Thok thrust action
     P_InstaThrust(player.mo, player.mo.angle, actionspd)
@@ -45,4 +47,17 @@ end)
 -- Reset the executed special flag when the player spawns
 addHook("PlayerSpawn", function(player)
     player.pflags = player.pflags & ~PF_EXECUTED_SPECIAL  -- Reset executed flag
+end)
+
+
+-- Separate ThinkFrame hook for maintaining flags
+addHook("ThinkFrame", function()
+    for player in players.iterate()
+        if player and player.mo and player.mo.valid then
+            -- Maintain the rolling animation if the character can't spindash
+            if not skins[player.mo.skin].abilities.spindash then
+                player.mo.state = S_PLAY_ROLL
+            end
+        end
+    end
 end)
