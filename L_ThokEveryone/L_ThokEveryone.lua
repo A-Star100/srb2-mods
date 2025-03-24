81a3e6b51a3876ccfafb7f8cdd28d0edfd9b30d6
Anirudh Sevugan <sonicforces207@gmail.com>
2025-03-24 22:11:58 +0530

    Update L_ThokEveryone.lua

--- L_ThokEveryone/L_ThokEveryone.lua
+++ L_ThokEveryone/L_ThokEveryone.lua
@@ -48,22 +48,18 @@ addHook("PlayerSpawn", function(player)
     player.pflags = player.pflags & ~PF_EXECUTED_SPECIAL  -- Reset executed flag
 end)
 
--- Remove this MobjMoveBlocked hook if you want to multithok
-addHook("MobjMoveBlocked", function(mo)
-    if mo.player then
-        mo.player.pflags = $ & ~PF_EXECUTED_SPECIAL
+-- Clear PF_EXECUTED_SPECIAL on landing
+addHook("PlayerThink", function(player)
+    if player.mo.eflags & MFE_TOUCHINGFLOOR then
+        player.pflags = $ & ~PF_EXECUTED_SPECIAL
     end
 end)
-
 
 -- Remove this ThinkFrame hook if you want the mod to work in 2.1
 addHook("ThinkFrame", function()
     for player in players.iterate()
         if player and player.mo and player.mo.valid then
-            -- Clear PF_EXECUTED_SPECIAL as well (fixes multi-thok)
             player.pflags = $ & ~PF_NOJUMPDAMAGE
-
-
         end
     end
 end)
